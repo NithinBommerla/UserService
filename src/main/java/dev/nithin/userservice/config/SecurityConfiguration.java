9b7776894ed1ff910101f692b3c74d6b991e5729
Nithin Bommerla <nithinbommerla99@gmail.com>
2025-06-26 21:52:45 +0530

    Added Eureka client for inter service communication through service discovery

--- src/main/java/dev/nithin/userservice/config/SecurityConfiguration.java
+++ src/main/java/dev/nithin/userservice/config/SecurityConfiguration.java
@@ -5,10 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-//@Configuration
+@Configuration
 public class SecurityConfiguration {
 
-//    @Bean
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> {
